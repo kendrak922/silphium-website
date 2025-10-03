bc571248df84922cc72082d48bbe9d75946e9d9e
Kendra Kyndberg <kendrak922@gmail.com>
2025-10-03 17:01:12 -0500

    adding border to container

--- wordpress/wp-content/themes/bansheeStarter/template-parts/blocks/_innerBlock/container/container.php
+++ wordpress/wp-content/themes/bansheeStarter/template-parts/blocks/_innerBlock/container/container.php
@@ -19,6 +19,7 @@ $blockData = array(
     'width' => get_field('width') ?? 'default',
     'vertical-align' => get_field('formatting_vertical_align') ?? 'top',
     'horizontal-align' => get_field('formatting_horizontal_align') ?? 'left',
+    'border_color' => get_field('add_border') ? get_field('border_color')['theme_colors'] : '',
 );
 
 // BLOCK :: CLASSES
@@ -26,22 +27,32 @@ $classes = [ 'inner-block--container' ];
 $classes[] = 'container';
 $classes[] = 'container--'.$blockData['width'];
 
-if($block['align']){
+
+// BLOCK :: STYLES
+$style = [];
+
+
+
+if($block['align']) {
     $classes[] = 'container--'.$block['align'];
 }
 
-if($block['align']){
+if($blockData['border_color']) {
+    $classes[] = 'u-borderColor'.$blockData['border_color'];
+}
+
+if($block['align']) {
     $classes[] = 'container--'.$block['align'];
 }
 
-if ( ! empty( $block['className'] ) ) {
-	$classes = array_merge( $classes, explode( ' ', $block['className'] ) );
+if (! empty($block['className']) ) {
+    $classes = array_merge($classes, explode(' ', $block['className']));
 }
 
 // BLOCK :: RENDER
 ?>
 
-<div id="<?php echo $blockID; ?>" class="inner-block <?php echo join( ' ', $classes ) ?>" data-align-x="<?php echo $blockData['horizontal-align']; ?>" data-align-y="<?php echo $blockData['vertical-align']; ?>" >
+<div id="<?php echo $blockID; ?>" class="inner-block <?php echo join(' ', $classes) ?>" data-align-x="<?php echo $blockData['horizontal-align']; ?>" data-align-y="<?php echo $blockData['vertical-align']; ?>" >
 
     <InnerBlocks 
         template="<?php echo esc_attr(wp_json_encode($blocks_template)); ?>" 
